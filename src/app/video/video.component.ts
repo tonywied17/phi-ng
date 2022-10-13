ea232c8182b682c7641dad3fca6b5b9bbcdbc0f7
Tony Wiedman <107732340+tonywied17@users.noreply.github.com>
2022-10-13 01:40:55 -0400

    More theming, new links json, etc

--- src/app/video/video.component.ts
+++ src/app/video/video.component.ts
@@ -28,40 +28,60 @@ export class VideoComponent implements AfterViewInit {
 
   }
 
-  /**
- * 
- */
+ /**
+  * 
+  * @param type 
+  */
   async links(type: String){
 
     let resp = await fetch('https://molex.cloud/phi/links.json?' + this.rando());
 
     if(resp.ok){
-      let streams = await resp.json();
+      
+      let json = await resp.json();
 
-      if(type == "phillies"){
-        console.log(streams.streams.phillies)
-        return streams.streams.phillies
-      }
+      console.log(json)
+      json.streams.forEach((element: any) => {
+        if(element.title == type){
+          console.log(element.url)
+          this.url = element.url
+        }
+      });
 
-      if(type == "eagles"){
-        console.log(streams.streams.eagles)
-        return streams.streams.eagles
-      }
     }
   }
+
+
   /**
-   * GET PHILLIES STREAM PAGE
+   * 
+   * @param stream 
    */
-  phillies(){
-    this.streamid="phillies" + this.rando();
-    this.isStreaming=true;
-    this.isEagles=false;
-    this.isPhillies=true;
+  streamSelector(stream: any){
+
+    if(stream == "phillies"){
+      this.isEagles=false;
+      this.isPhillies=true;
+    }
     
+    if(stream == "eagles"){
+
+      this.isEagles=true;
+      this.isPhillies=false;
+
+    }
+
+    if(stream == "extra1"){
+
+      this.isEagles=false;
+      this.isPhillies=false;
+
+    }
+
+    this.streamid=stream + this.rando();
+    this.isStreaming=true;
+    this.links(stream)
 
     setTimeout(async () => {
-      console.log(this.links("phillies"))
-      this.url = await this.links("phillies");
 
       this.player = videojs(this.streamid, {
         aspectRatio: '4:3',
@@ -84,62 +104,11 @@ export class VideoComponent implements AfterViewInit {
         this.player.controls(true);
       });
     }, 300)
-    this.openSnackBar('Channel changed to phillies', 'Close');
-}
+    setTimeout(() => {
+      this.openSnackBar('Channel changed to phillies', 'Close');
+    }, 300)
+  }
 
- /**
-   * GET EAGLES STREAM PAGE
-   */
-eagles(){
-  
-  this.streamid="eagles" + this.rando();;
-  this.isStreaming=true;
-  this.isEagles=true;
-  this.isPhillies=false;
-  
-
-  setTimeout(async () => {
-    console.log(this.links("eagles"))
-    this.url = await this.links("eagles");
-
-  this.player = videojs(this.streamid, {
-    aspectRatio: '4:3',
-    controls: true,
-    autoplay: true,
-    muted: true,
-    html5: {
-      vhs: {
-        overrideNative: true
-      }
-    }
-  });
-
-  this.player.src({
-    src: this.url,
-    type: 'application/x-mpegURL'
-  });
-
-  this.player.on('play', () => {
-    this.player.controls(true);
-  });
-  }, 300)
-  this.openSnackBar('Channel changed to eagles', 'Close');
-}
-
-/**
- * 
- */
-redzone(){
-  this.openSnackBar('No stream available', 'Close');
-}
-
-
-/**
- * 
- */
-extra(){
-  this.openSnackBar('Extra 1 not available', 'Close');
-}
 
 theme(theme: any){
   if(theme){
